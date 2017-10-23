749148071008db9f3b2b887ab7cbdd39f2eb78f9
Zaid Huda <tripledot99@gmail.com>
2017-10-23 23:02:25 +0800

    some changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
   def session_link
     if current_user
-      active_link_to t('actions.logout'), logout_path, method: :delete, class: 'nav-link', wrap_tag: :li, wrap_class: 'nav-item'
+      active_link_to t('actions.logout'), logout_path, method: :delete,
+        class: 'nav-link', wrap_tag: :li, wrap_class: 'nav-item', data: { confirm: t('hints.user.logout') }
     else
       active_link_to t('actions.login'), login_path, class: 'nav-link', wrap_tag: :li, wrap_class: 'nav-item'
     end
